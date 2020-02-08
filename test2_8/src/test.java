63427e09be4de59040aa4cd2720a2db340e5dd92
lym <949197083@qq.com>
2020-02-08 21:56:47 +0800

    添加了test方法

--- test2_8/src/test.java
+++ test2_8/src/test.java
@@ -2,4 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("创建项目");
     }
+    public void test(){
+        System.out.println("add test method");
+    }
 }
